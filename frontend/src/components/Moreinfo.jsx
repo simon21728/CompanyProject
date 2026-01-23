63877ac3a2ea89fd6e01277cc43827dbd7eb0199
Simon <simonbayu2211@exgmail.com>
2026-01-23 14:15:43 +0300

    pading

--- frontend/src/components/Moreinfo.jsx
+++ frontend/src/components/Moreinfo.jsx
@@ -1,15 +1,10 @@
 function Moreinfo() {
   return (
     <div className="min-h-screen bg-gray-800 text-white m-0 p-0">
-          style={{
-              padding: "50px 20px",
-              background: "linear-gradient(to bottom, #f0f4f8, #e0f7fa)",
-              minHeight: "100vh",
-              fontFamily: "Arial, sans-serif", }}
       <div
         className="mt-[20px] ml-[30px]"
         style={{
-              padding: "50px 20px",
+              padding: "180px 500px",
               background: "linear-gradient(to bottom, #f0f4f8, #e0f7fa)",
               minHeight: "100vh",
               fontFamily: "Arial, sans-serif", }}
