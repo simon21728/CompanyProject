33bebd99a74c89fb16741234fb42de95e504b42e
Simon <simonbayu2211@exgmail.com>
2026-01-23 11:23:58 +0300

    fix

--- backend/company_backend/settings.py
+++ backend/company_backend/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,12 +65,17 @@ MIDDLEWARE = [
 
 
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://companyproject-k4hz.onrender.com",  # Vite default dev server
 ]
 
+
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+]
 ROOT_URLCONF = 'company_backend.urls'
 
 TEMPLATES = [
